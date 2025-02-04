1bb32ab7fe9de6491a099be045133fecd04297a1
Mr-Lime1Em <omar.gulu2010@gmail.com>
2025-02-04 16:33:50 +0400

    first

---
+++ carSale/src/main/java/com/example/carSale/controller/CarController.java
@@ -0,0 +1,38 @@
+package com.example.carSale.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.carSale.entity.CarEntity;
+import com.example.carSale.service.CarService;
+
+@RestController
+@RequestMapping("/cars")
+public class CarController {
+
+    @Autowired
+    CarService carService;
+
+    @GetMapping("/list")
+    public List<CarEntity> findAll() {
+        return carService.findAllCars();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<CarEntity> findById(@PathVariable Integer id) {
+        return carService.findByIdCars(id);
+    }
+
+    @PostMapping("/add")
+    public void save(@RequestBody CarEntity carEntity) {
+        carService.saveCar(carEntity);
+    }
+}
