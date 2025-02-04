b177f661b5ef0be60ad26e9f077a85937a4c21ce
Mr-Lime1Em <omar.gulu2010@gmail.com>
2025-02-04 16:56:57 +0400

    Update CarController.java

--- carSale/src/main/java/com/example/carSale/controller/CarController.java
+++ carSale/src/main/java/com/example/carSale/controller/CarController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.carSale.entity.CarEntity;
@@ -32,6 +34,7 @@ public class CarController {
     }
 
     @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
     public void save(@RequestBody CarEntity carEntity) {
         carService.saveCar(carEntity);
     }
