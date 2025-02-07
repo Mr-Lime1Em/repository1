705dbbecd760f91a5978c5f1d3c817f53e9be109
Mr-Lime1Em <omar.gulu2010@gmail.com>
2025-02-07 15:35:25 +0400

    Update CarController.java

--- carSale/src/main/java/com/example/carSale/controller/CarController.java
+++ carSale/src/main/java/com/example/carSale/controller/CarController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.example.carSale.service.CarService;
 
 @RestController
 @RequestMapping("/cars")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class CarController {
 
     @Autowired
